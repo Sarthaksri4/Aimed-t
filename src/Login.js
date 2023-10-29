9609a0d1eac572e858add903a98f59c33bfe650c
sarthak <sarthaksri044@gmail.com>
2023-10-29 22:24:12 +0530

    final

--- src/Login.js
+++ src/Login.js
@@ -1,99 +1,308 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Photo from './ph.png';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import styled from 'styled-components';
-import Logo from './ph.png';
 
-const Login = () => {
-  return (
-    <Container>
-      <Image src={Logo} alt="Login Image" />
-      <LoginForm>
-        <h2>Login</h2>
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <RememberMe>
-          <input type="checkbox" /> Remember me
-        </RememberMe>
-        <AgreeToTerms>
-          <input type="checkbox" /> Agree to Terms and Conditions
-        </AgreeToTerms>
-        <ChangePassword>Forgot Password?</ChangePassword>
-        <SubmitButton>Log In</SubmitButton>
-        <SignupLink>Don't have an account? Sign up</SignupLink>
-      </LoginForm>
-    </Container>
-  );
-};
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
+const CustomContainer = styled.div`
+ display: inline-flex;
+  padding: 40px 40px 40px 142px;
+  justify-content: flex-end;
   align-items: center;
-  height: 100vh;
+  gap: 147px;
+  background: #FFF;
+
+  @media (max-width: 998px) {
+    padding: 1rem 3.2rem;
+  }
+
   @media (max-width: 768px) {
     flex-direction: column;
+    padding: 2rem;
+  }
+
+  @media (max-width: 480px) {
+    padding: 3rem;
   }
 `;
 
-const Image = styled.img`
-  max-width: 60%;
-  height: 60%;
-  margin-right: 180px;
+const CustomLeftPanel = styled.div`
+  width: 30%;
+  display: flex;
+  align-items: center;
+
   @media (max-width: 768px) {
-    margin-bottom: 20px;
-    margin-right: 0;
+    width: 70%;
+    margin-bottom: -5rem;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
   }
 `;
 
-const LoginForm = styled.form`
-  width:40%;
-  height:70%;
-  border: 1px solid #ccc;
-  padding: 20px;
+const CustomImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const CustomRightPanel = styled.div`
+  width: 70%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
   border-radius: 12px;
   background: #FFF;
-box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.16);
+  box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.16);
+
   @media (max-width: 768px) {
-    width: 80%;
+    width: 100%;
+    height:65vh;
+    margin-left:30px;
   }
 `;
 
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin: 10px 0;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+const CustomForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px;
+
+  @media (max-width: 480px) {
+    padding: 2rem 1rem;
+  }
 `;
 
-const RememberMe = styled.label`
+const CustomPasswordToggle = styled.div`
+  position: absolute;
+  right: 35px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    right: 20px;
+  }
+`;
+
+const CustomH2 = styled.h2`
+  color: #04072F;
+  text-align: center;
+  font-family: Poppins;
+  font-size: 3rem;
+  font-style: normal;
+  font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+const CustomLabel = styled.label`
+  text-align: left;
+  color: #04072F;
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 110%;
+  margin-left: 1rem;
+
+  @media (max-width: 480px) {
+    line-height: 90%;
+  }
+`;
+
+const CustomInputContainer = styled.div`
   display: flex;
   align-items: center;
-  margin: 10px 0;
+  gap: 8px;
+  position: relative;
 `;
 
-const AgreeToTerms = styled.label`
+const CustomInput = styled.input`
+  padding: 8px;
+  width: 90%;
+  border-radius: 8px;
+  border: 1px solid rgba(4, 7, 47, 0.4);
+  margin-left: 1rem;
+`;
+
+const CustomPasswordInput = styled.input`
+  padding: 8px;
+  width: 90%;
+  border-radius: 8px;
+  border: 1px solid rgba(4, 7, 47, 0.4);
+  margin-left: 1rem;
+`;
+
+const CustomCheckboxContainer = styled.div`
   display: flex;
   align-items: center;
-  margin: 10px 0;
+  gap: 8px;
+  margin-left: 1rem;
+
+  @media (max-width: 768px) {
+    gap: 1px;
+  }
 `;
 
-const ChangePassword = styled.span`
-  color: #0077FF;
+const CustomCheckbox = styled.input`
+  margin: 0;
+`;
+
+const CustomButton = styled.button`
+  padding: 12px;
+  margin:12px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  align-self: center;
+  border-radius: 8px;
+  background: #1575A7;
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 110%;
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+    width: 95%;
+    margin-left: 10px;
+  }
+`;
+
+const CustomRegisterLink = styled.a`
+  text-align: center;
+  text-decoration: none;
+  cursor: default;
+  color: #04072F;
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const CustomChangePasswordLink = styled.a`
+  text-align: center;
+  margin-left: auto;
+  margin-right: 2rem;
+  text-decoration: none;
+  cursor: pointer;
+  color: #F78719;
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 110%;
+
+  @media (max-width: 768px) {
+    margin-right: 0.5rem;
+  }
+`;
+
+const CustomStyledSpan = styled.span`
+  color: #F78719;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
   text-decoration: underline;
   cursor: pointer;
 `;
 
-const SubmitButton = styled.button`
-  background-color: #0077FF;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`;
+function CustomLogin() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [customPassword, setCustomPassword] = useState('');
+  const [customLoginId, setCustomLoginId] = useState('');
+  const [customRemember, setCustomRemember] = useState(false);
+  const [customTerms, setCustomTerms] = useState(false);
 
-const SignupLink = styled.div`
-  margin: 20px 0;
-`;
+  const toggleCustomPasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
-export default Login;
+  const handleCustomSubmit = (e) => {
+    e.preventDefault();
+    console.log('Custom Login ID:', customLoginId);
+    console.log('Custom Password:', customPassword);
+    setCustomLoginId('');
+    setCustomPassword('');
+    setCustomRemember(false);
+    setCustomTerms(false);
+  };
+
+  return (
+    <CustomContainer>
+      <CustomLeftPanel>
+        <CustomImage src={Photo} alt="LoginImage" />
+      </CustomLeftPanel>
+      <CustomRightPanel>
+        <CustomForm onSubmit={handleCustomSubmit}>
+          <CustomH2>Login</CustomH2>
+          <CustomLabel htmlFor="customLoginId">Login ID</CustomLabel>
+          <CustomInput
+            type="text"
+            id="customLoginId"
+            name="customLoginId"
+            value={customLoginId}
+            placeholder="Enter Login ID"
+            required
+            onChange={(e) => setCustomLoginId(e.target.value)}
+          />
+          <CustomLabel htmlFor="customPassword">Password</CustomLabel>
+          <CustomInputContainer>
+            <CustomPasswordInput
+              type={passwordVisible ? 'text' : 'password'}
+              id="customPassword"
+              placeholder="Enter Password"
+              name="customPassword"
+              required
+              value={customPassword}
+              onChange={(e) => setCustomPassword(e.target.value)}
+            />
+            <CustomPasswordToggle onClick={toggleCustomPasswordVisibility}>
+              {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+            </CustomPasswordToggle>
+          </CustomInputContainer>
+          <CustomCheckboxContainer>
+            <CustomCheckbox
+              type="checkbox"
+              id="customRemember"
+              name="customRemember"
+              checked={customRemember}
+              required
+              onChange={() => setCustomRemember(!customRemember)}
+            />
+            <CustomLabel htmlFor="customRemember">Remember me</CustomLabel>
+            <CustomChangePasswordLink href="/custom-password">
+              Change Password
+            </CustomChangePasswordLink>
+          </CustomCheckboxContainer>
+          <CustomCheckboxContainer>
+            <CustomCheckbox
+              type="checkbox"
+              id="customTerms"
+              name="customTerms"
+              checked={customTerms}
+              required
+              onChange={() => setCustomTerms(!customTerms)}
+            />
+            <CustomLabel htmlFor="customTerms">
+              Agree to the <a href="/custom-terms-conditions" style={{ color: '#F78719' }}>Terms and Conditions</a>
+            </CustomLabel>
+          </CustomCheckboxContainer>
+          <CustomButton type="submit">Login</CustomButton>
+          <CustomRegisterLink href="/custom-register">
+            Don't have an account? <CustomStyledSpan>Register Here</CustomStyledSpan>
+          </CustomRegisterLink>
+        </CustomForm>
+      </CustomRightPanel>
+    </CustomContainer>
+  );
+}
+
+export default CustomLogin;
